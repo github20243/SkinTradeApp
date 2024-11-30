4381664ee845ae9edf341519ee5c9ba22e922dd7
nurs_996 <98372@gamil.com>
2024-11-30 16:53:41 +0600

    fix fontWeight 400

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,29 +19,29 @@ const Header = () => {
         <NavWrapper>
           <NavList>
             <NavItem>
-              <Typography>ПРОДАТЬ СКИНЫ</Typography>
+              <StyledNavItemText>ПРОДАТЬ СКИНЫ</StyledNavItemText>
             </NavItem>
             <NavItem>
-              <Typography sx={{ color: "#007AFF" }}>КУПИТЬ СКИНЫ</Typography>
+              <StyledNavItemText color="#007AFF">КУПИТЬ СКИНЫ</StyledNavItemText>
             </NavItem>
             <NavItem>
-              <Typography>FAQ</Typography>
+              <StyledNavItemText>FAQ</StyledNavItemText>
             </NavItem>
             <NavItem>
-              <Typography>ГАРАНТИИ</Typography>
+              <StyledNavItemText>ГАРАНТИИ</StyledNavItemText>
             </NavItem>
             <NavItem>
-              <Typography>БЛОГ</Typography>
+              <StyledNavItemText>БЛОГ</StyledNavItemText>
             </NavItem>
             <NavItem>
-              <Typography sx={{ color: "#AD95FF" }}>GIVEAWAY</Typography>
+              <StyledNavItemText color="#AD95FF">GIVEAWAY</StyledNavItemText>
             </NavItem>
           </NavList>
         </NavWrapper>
         <IconsWrapper>
           <StyledContainer>
-          <StyledYoutubeIcon src={YoutubeLogo} alt="YouTube" />
-          <StyledTelegramIcon src={TelegramLogo} alt="Telegram" />
+            <StyledYoutubeIcon src={YoutubeLogo} alt="YouTube" />
+            <StyledTelegramIcon src={TelegramLogo} alt="Telegram" />
           </StyledContainer>
           <SteamButton>
             <StyledSteamIcon src={steamIcons} alt="Steam" />
@@ -55,25 +55,44 @@ const Header = () => {
 
 export default Header;
 
-const HeaderWrapper = styled(AppBar)({
+const HeaderWrapper = styled(AppBar)(({ theme }) => ({
   backgroundColor: "#1A1C21",
   color: "white",
-});
+  [theme.breakpoints.down("xs")]: {
+    padding: "0 10px",
+  },
+}));
 
-const StyledLogo = styled("img")({
+const StyledLogo = styled("img")(({ theme }) => ({
   width: "62px",
   height: "61px",
   position: "absolute",
   left: "165px",
-});
+  [theme.breakpoints.down("md")]: {
+    width: "50px",
+    height: "50px",
+    left: "20px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    width: "40px",
+    height: "40px",
+    left: "10px",
+  },
+}));
 
-const NavWrapper = styled(Box)({
+const NavWrapper = styled(Box)(({ theme }) => ({
   flexGrow: 1,
   display: "flex",
   justifyContent: "center",
-});
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "flex-start",
+  },
+  [theme.breakpoints.down("xs")]: {
+    justifyContent: "center",
+  },
+}));
 
-const NavList = styled("ul")({
+const NavList = styled("ul")(({ theme }) => ({
   display: "flex",
   listStyle: "none",
   gap: "25px",
@@ -84,15 +103,24 @@ const NavList = styled("ul")({
   justifyContent: "center",
   width: "800px",
   marginLeft: "40px",
-  "& .active": {
-    color: "#007AFF",
+  [theme.breakpoints.down("md")]: {
+    width: "600px",
   },
-});
+  [theme.breakpoints.down("sm")]: {
+    width: "400px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    flexDirection: "column",
+    gap: "10px",
+    width: "auto",
+    marginLeft: 0,
+  },
+}));
 
-const NavItem = styled("li")({
+const NavItem = styled("li")(({ theme }) => ({
   cursor: "pointer",
   fontSize: "14px",
-  fontWeight: 400,
+  fontWeight: 300,
   lineHeight: "14px",
   paddingRight: "6.6px",
   "&:hover": {
@@ -104,9 +132,27 @@ const NavItem = styled("li")({
     color: "#007AFF",
     fontWeight: 600,
   },
-});
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "12px",
+  },
+}));
 
-const SteamButton = styled(Button)(({
+const StyledNavItemText = styled(Typography)(({ theme, color }) => ({
+  fontWeight: 400,
+  fontSize: "14px",
+  lineHeight: "14px",
+  color: color || "inherit",
+  "&:hover": {
+    color: "#1e90ff",
+    borderBottom: "2px solid #1e90ff",
+    transition: "border-bottom 0.3s ease",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "12px",
+  },
+}));
+
+const SteamButton = styled(Button)(({ theme }) => ({
   width: "200px",
   height: "38px",
   borderRadius: "4px",
@@ -118,19 +164,36 @@ const SteamButton = styled(Button)(({
   fontFamily: "'Open Sans', sans-serif",
   position: "relative",
   right: "80px",
+  [theme.breakpoints.down("sm")]: {
+    right: "10px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    width: "150px",
+    right: "0px",
+    fontSize: "10px",
+  },
   "&:hover": {
     backgroundColor: "#0056b3",
   },
 }));
 
-const IconsWrapper = styled(Box)({
+const IconsWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-start",
   marginLeft: "120px",
   flexDirection: "row-reverse",
   gap: "10px",
-});
+  [theme.breakpoints.down("md")]: {
+    marginLeft: "40px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    flexDirection: "column",
+    gap: "5px",
+    marginLeft: "0",
+    marginTop: "10px",
+  },
+}));
 
 const Icon = styled("img")({
   width: "18px",
@@ -156,11 +219,17 @@ const StyledTelegramIcon = styled(Icon)({
   cursor: "pointer",
 });
 
-
-const StyledContainer = styled("div")(({}) => ({
+const StyledContainer = styled("div")(({ theme }) => ({
   position: "relative",
   right: "375px",
   display: "flex",
   justifyContent: "center",
-  gap: "10px"
-}))
+  gap: "10px",
+  [theme.breakpoints.down("md")]: {
+    right: "50px",
+  },
+  [theme.breakpoints.down("xs")]: {
+    right: "0",
+    justifyContent: "flex-start",
+  },
+}));
